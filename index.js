d4d02d928ebcee1a20179280b51f570c3c8a1c16
Huy Hung <hungro8102@gmail.com>
2024-04-22 02:20:59 +0700

    test workflow again

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Checkinwork");
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  
+  return (
+   <View>
+    <Text>Github-action</Text>
+   </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  
+  },
+});
